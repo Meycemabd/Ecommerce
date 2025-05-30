0cdcea9bc40fa430a30e6d7c706778c8ade53fe5
p.meycem.a <meycemelabed@gmail.com>
2025-05-30 19:26:21 +0200

    redux config

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './cartSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import favoritesReducer from "./favoritesSlice";
 
 export const store = configureStore({
   reducer: {
-    cart: cartReducer,
+    favorites: favoritesReducer,
   },
 });
